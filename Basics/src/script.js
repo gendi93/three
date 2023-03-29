9bf528908e660aaf162209f57664702a48d17428
Yehia El Gendi <yehia.gendi@gmail.com>
2023-03-28 23:47:14 -0400

    Add animation

--- Basics/src/script.js
+++ Basics/src/script.js
@@ -1,27 +1,38 @@
-import * as THREE from 'three'
+import {
+    Scene,
+    Group,
+    Mesh,
+    BoxGeometry,
+    MeshBasicMaterial,
+    PerspectiveCamera,
+    WebGLRenderer,
+    Clock,
+    AxesHelper
+} from 'three';
+import gsap from 'gsap';
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector('canvas.webgl');
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new Scene();
 
 /**
  * Objects
  */
-const group = new THREE.Group();
+const group = new Group();
 
-const cube1 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const cube1 = new Mesh(
+    new BoxGeometry(1, 1, 1),
+    new MeshBasicMaterial({ color: 0xff0000 })
 );
-const cube2 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+const cube2 = new Mesh(
+    new BoxGeometry(1, 1, 1),
+    new MeshBasicMaterial({ color: 0x00ff00 })
 );
-const cube3 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0x0000ff })
+const cube3 = new Mesh(
+    new BoxGeometry(1, 1, 1),
+    new MeshBasicMaterial({ color: 0x0000ff })
 );
 cube1.position.set(-1.5, 0, 0);
 cube3.position.set(1.5, 0, 0);
@@ -30,9 +41,9 @@ group.add(cube1);
 group.add(cube2);
 group.add(cube3);
 
-scene.add(group)
+scene.add(group);
 
-const axesHelper = new THREE.AxesHelper(0.2);
+const axesHelper = new AxesHelper(0.2);
 scene.add(axesHelper);
 
 /**
@@ -41,20 +52,42 @@ scene.add(axesHelper);
 const sizes = {
     width: 800,
     height: 600
-}
+};
 
 /**
  * Camera
  */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
-scene.add(camera)
+const camera = new PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
 
 /**
  * Renderer
  */
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+const renderer = new WebGLRenderer({ canvas: canvas });
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
+
+/**
+ * Animate
+ */
+
+gsap.to(group.rotation, { duration: 1, delay: 1, x: 2 });
+gsap.to(group.rotation, { duration: 1, delay: 2, x: 0 });
+
+
+// const clock = new Clock();
+
+const animate = () => {
+    // const elapsedTime = clock.getElapsedTime();
+
+    // group.position.x = -Math.sin(elapsedTime);
+    // camera.position.x = Math.sin(elapsedTime);
+    // camera.lookAt(group.position)
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(animate);
+}
+
+animate();
