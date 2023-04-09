92e54e02905fb961ea3b545d1263923ddc93ba4d
Yehia El Gendi <yehia.gendi@gmail.com>
2023-04-09 18:54:13 -0400

    Create haunted scene with ghosts

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
 
 export default {
-    root: './2-ClassicTechniques/16-Shadows/src/',
+    root: './2-ClassicTechniques/17-HauntedHouse/src/',
     publicDir: '../static/',
     base: './',
     server:
