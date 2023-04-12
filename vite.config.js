b68cf8faf0fdb4cd1d770e9a43f800e885bace5b
Yehia El Gendi <yehia.gendi@gmail.com>
2023-04-12 02:17:40 -0400

    Restructure files and folders to navigate different scenes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env;
 
 export default {
-  root: './2-ClassicTechniques/18-ParticlesWave/src/',
+  root: './src/',
   publicDir: '../static/',
   base: './',
   server:
